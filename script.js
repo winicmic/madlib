939a7dbebf81220f59fc62191418843cf5b883c2
winicmic <53783136+winicmic@users.noreply.github.com>
2019-11-19 17:36:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function madlIb(){
+
+let word1 = document.getElementById("word1").value;
+let word2 = document.getElementById("word2").value;
+let word3 = document.getElementById("word3").value;
+let word4 = document.getElementById("word4").value;
+let word5 = document.getElementById("word5").value;
+let word6 = document.getElementById("word6").value;
+let word7 = document.getElementById("word7").value;
+let word8 = document.getElementById("word8").value;
+let word9 = document.getElementById("word9").value;
+let word10 = document.getElementById("word10").value;
+let word11 = document.getElementById("word11").value;
+let word12 = document.getElementById("word12").value;
+let word13 = document.getElementById("word13").value;
+let word14 = document.getElementById("word14").value;
+let word15 = document.getElementById("word15").value;
+
+
+document.getElementById("frontPage").innerHTML = 
+"<h1>The Walmart Experience</h1> <br><br> <p>Come " + word1 + " at Walmart, where you'll recieve " + word2 + " discounts on all of your favorite brand name " + word3 + ". Our " + word4 + " and " + word5 + " associates are there to " + word6 + " you " + word7 + " hours a day. Here you will find " + word8 + " prices on all the " + word9 + " you need. " + "<span>"+word10+"</span>" + " for the moms, " + word11 + " for the kids, and all the latest electronics for the " + word12 + ". So come on down to your " + word13 + ", " + word14 + " Walmart, where the " + word15 + " come first.</p> <br><br>";
+
+
+
+}
